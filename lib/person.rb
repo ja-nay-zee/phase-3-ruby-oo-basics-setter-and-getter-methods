cd723bd4ded989ddd316928aea5093e1ec33e589
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-22 14:27:22 -0400

    commit tree 071c39c9203183443a1a636057d441a1579abcdb

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    def name=(word)
+        @name = word
+    end
+
+    def name 
+       @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+   
+    def job
+      @job
+    end
 
 end
